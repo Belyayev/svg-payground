2822fe88e06e8572d73d3244ae146c82dad69cb1
Mike <4xgood@gmail.com>
2022-01-18 13:54:06 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ReactComponent as Logo } from "./svg/lines.svg";
+import lines from "./svg/lines.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SVG playground</h1>
+      <div>
+        <img src={lines} alt="lines" />
+        <Logo x1={50} y1={44} />
+      </div>
     </div>
   );
 }
