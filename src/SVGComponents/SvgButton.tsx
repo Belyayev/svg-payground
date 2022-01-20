10122a739a5883ff81ef3038989c02e65fb6086f
Mike Belyayev <4xgood@gmail.com>
2022-01-20 14:56:30 -0700

    added svg button skeletons

--- src/SVGComponents/SvgButton.tsx
+++ src/SVGComponents/SvgButton.tsx
@@ -4,20 +4,22 @@ interface ISvg {
 }
 
 function SvgButton(props: ISvg) {
-  let x = props.x;
-  let y = props.y;
+  let x = 200;
+  let y = 50;
 
   return (
     <div>
       <svg
         display="block"
-        width="200"
-        height="50"
+        width={x}
         viewBox="0 0 200 50"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect width="200" height="50" fill="lime" />
+        <rect width={x} height={y} fill="lime" />
+        <text text-anchor="middle" x={x / 2} y={y / 2 + 5} fill="black">
+          Text
+        </text>
       </svg>
     </div>
   );
