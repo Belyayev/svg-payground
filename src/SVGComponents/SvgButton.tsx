8eb415d70092d417991ecc656ab3db6f9fc1b33f
Mike <4xgood@gmail.com>
2022-01-22 18:16:22 -0700

    added commentary

--- src/SVGComponents/SvgButton.tsx
+++ src/SVGComponents/SvgButton.tsx
@@ -1,5 +1,16 @@
-import { useState } from "react";
+/*
+Sci-Fi styled react component button
+made using SVG with animations on hover and click
+it takes optional parameters for size, color and text
+all functionality is packed into single component file
+--------------------------------------------
+import SvgButton from "./SvgButton";
+<SvgButton text="home" color="red" x={100} y={30} />
+--------------------------------------------
+https://github.com/Belyayev/svg-payground
+*/
 
+import { useState } from "react";
 interface ISvg {
   x?: number;
   y?: number;
--- src/SVGComponents/SvgComponent.tsx
+++ src/SVGComponents/SvgComponent.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 interface ISvg {
   x: number;
   y: number;
